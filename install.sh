5340146cd329f89f2588e5fc6e1c24bd9bd68134
Daryl L. L. Houston <daryl@learnhouston.com>
2024-12-02 22:33:51 -0500

    Add code to update permissions on a serial port so that the software can connect. Also some other small tweaks to other bits.

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-PABLO_DIRECTORY=~/pablo
+#############################################################################
+# Run using sudo so that things requiring elevated permissions can run.
+#
+#
+
+
+
+USERNAME=`logname`
+HOMEDIR="/home/$USERNAME"
+PABLO_DIRECTORY="$HOMEDIR/pablo"
 
 #############################################################################
 # Make a working directory or alert if one already exists.
@@ -9,8 +18,10 @@ PABLO_DIRECTORY=~/pablo
 
 if [ -e $PABLO_DIRECTORY ]
 then
-	echo "$PABLO_DIRECTORY already exists."
 	echo ""
+	echo "$PABLO_DIRECTORY already exists. Exiting."
+	echo ""
+	exit
 else
 	mkdir $PABLO_DIRECTORY
 	echo "Created $PABLO_DIRECTORY"
@@ -18,7 +29,8 @@ else
 fi
 
 cd $PABLO_DIRECTORY
-mkdir -p ~/sketchbook/libraries
+
+mkdir -p $HOMEDIR/sketchbook/libraries
 
 
 #############################################################################
@@ -57,8 +69,10 @@ then
 fi
 
 unzip -o polargraph.zip -d $PABLO_DIRECTORY
-mv $PABLO_DIRECTORY/Polargraph\ 2017-11-01/processing-source/Processing\ libraries/* ~/sketchbook/libraries
-mv $PABLO_DIRECTORY/Polargraph\ 2017-11-01/processing-source/polargraphcontroller ~/sketchbook
+echo "Moving polargraph libraries and source to $HOMEDIR/sketchbook"
+mv $PABLO_DIRECTORY/Polargraph\ 2017-11-01/processing-source/Processing\ libraries/* $HOMEDIR/sketchbook/libraries
+mv $PABLO_DIRECTORY/Polargraph\ 2017-11-01/processing-source/polargraphcontroller/ $HOMEDIR/sketchbook
+chown -R $USERNAME $HOMEDIR/sketchbook
 
 
 #############################################################################
@@ -66,18 +80,18 @@ mv $PABLO_DIRECTORY/Polargraph\ 2017-11-01/processing-source/polargraphcontrolle
 #
 #
 
-if [ -e ~/Desktop/Pablo.desktop ]
+if [ -e $HOMEDIR/Desktop/Pablo.desktop ]
 then
 	echo "Pablo desktop shortcut already exists. Not overwriting"
 	echo ""
 else
-	read -r -d '' DESKTOP_SHORTCUT <<'EOF'
+	read -r -d '' DESKTOP_SHORTCUT <<EOF
 [Desktop Entry]
 Type=Application
 Name=Pablo
 GenericName=Pablo
 Comment=Open-source software prototyping platform
-Exec=~/pablo/processing-2.2.1/processing ~/sketchbook/polargraphcontroller/polargraphcontroller.pde
+Exec=$HOMEDIR/pablo/processing-2.2.1/processing $HOMEDIR/sketchbook/polargraphcontroller/polargraphcontroller.pde
 Icon=processing-pde
 Terminal=false
 Categories=Development;IDE;Programming;
@@ -86,15 +100,46 @@ Keywords=sketching;software;animation;programming;coding;
 StartupWMClass=processing-app-ui-Splash
 EOF
 
-	echo "$DESKTOP_SHORTCUT" > ~/Desktop/Pablo.desktop
-	chmod ugo+x ~/Desktop/Pablo.desktop
+	echo "$DESKTOP_SHORTCUT" > $HOMEDIR/Desktop/Pablo.desktop
+	chmod ugo+x $HOMEDIR/Desktop/Pablo.desktop
 fi
 
+#############################################################################
+# Set permissions for accessing the serial port.
+#
+#
+
+if [ -e "$PABLO_DIRECTORY/serial-setup" ]; then
+	echo ""
+	echo "Serial permissions have already been set. Not resetting."
+	echo ""
+else
+	touch "$PABLO_DIRECTORY/serial-setup"
+	UDEV_PATH="/etc/udev/rules.d"
+	UDEV_FILE_ROOT="usb-serial.rules"
+
+	read -r -d '' UDEV_CONFIG <<'EOM'
+KERNEL=="ttyACM[0-9]*",MODE="0666"\nKERNEL=="ttyS[0-9]*",MODE="0666"\nKERNEL=="ttyUSB[0-9]*",MODE="0666"
+EOM
+
+	for i in {10..31}; do
+		UDEV_FILE="$UDEV_PATH/$i-$UDEV_FILE_ROOT"
+        
+		if [[ ! -e "$UDEV_FILE" ]]; then
+			echo "Will write file: $UDEV_FILE"
+			echo -e $UDEV_CONFIG > $UDEV_FILE
+			break
+        	fi
+	done
+fi
+
+udevadm control --reload
 
 #############################################################################
 # Cleanup
 #
 #
 
-rm -rf ~/pablo/'Polargraph 2017-11-01'
-rm -rf ~/pablo/polargraph.zip
+chown -R $USERNAME $PABLO_DIRECTORY
+rm -rf $PABLO_DIRECTORY/'Polargraph 2017-11-01'
+rm -rf $PABLO_DIRECTORY/polargraph.zip
